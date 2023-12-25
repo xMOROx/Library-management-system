fec9783b8d61c0a00482984029d202471ab3b82d
Piotr Kuchta <pkuchta@student.agh.edu.pl>
2023-12-26 00:57:51 +0100

    Modify IssueBookController

--- Management-Library-System/src/main/java/pl/edu/agh/managementlibrarysystem/controller/IssueBookController.java
+++ Management-Library-System/src/main/java/pl/edu/agh/managementlibrarysystem/controller/IssueBookController.java
@@ -2,19 +2,27 @@ package pl.edu.agh.managementlibrarysystem.controller;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import pl.edu.agh.managementlibrarysystem.DTO.BookDTO;
 import pl.edu.agh.managementlibrarysystem.DTO.UserDTO;
 import pl.edu.agh.managementlibrarysystem.controller.abstraction.ResizeableBaseController;
+import pl.edu.agh.managementlibrarysystem.model.User;
+import pl.edu.agh.managementlibrarysystem.model.util.Permission;
 import pl.edu.agh.managementlibrarysystem.service.BookService;
 import pl.edu.agh.managementlibrarysystem.service.UserService;
+import pl.edu.agh.managementlibrarysystem.session.UserSession;
 import pl.edu.agh.managementlibrarysystem.utils.Alerts;
+import pl.edu.agh.managementlibrarysystem.utils.ControlsUtils;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,6 +32,7 @@ import java.util.ResourceBundle;
 public class IssueBookController extends ResizeableBaseController implements Initializable {
     private final BookService bookService;
     private final UserService userService;
+    private final UserSession session;
 
     @FXML
     private Text bookTitle;
@@ -45,36 +54,71 @@ public class IssueBookController extends ResizeableBaseController implements Ini
     private MFXTextField bookSearchField;
     @FXML
     private MFXTextField numberOfDaysTextField;
+    @FXML
+    private Label errorISBNLabel;
+    @FXML
+    private Label errorUserLabel;
 
     private BookDTO book;
     private UserDTO user;
 
+    StringProperty errorISBNMessage;
+    StringProperty errorUserMessage;
+
+    Long userId;
+    boolean logged;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         tooltipInitializer();
+        errorISBNMessage = new SimpleStringProperty();
+        errorISBNLabel.textProperty().bind(errorISBNMessage);
+        errorUserMessage = new SimpleStringProperty();
+        errorUserLabel.textProperty().bind(errorUserMessage);
+        initializeStageOptions();
         this.issueBook.setDisable(true);
+    }
+
+    private void initializeStageOptions(){
+        if(session.getLoggedUser()==null){
+            return;
+        }
+        User u = session.getLoggedUser();
+        if(u.getPermission() == Permission.NORMAL_USER){
+            userId = session.getLoggedUser().getId();
+            ControlsUtils.changeControlVisibility(userSearchTextField,false);
+            user = userService.findById(this.userId);
+            logged = true;
+            setUserControls(user);
+        }
+        else{
+            logged = false;
+        }
     }
 
     @FXML
     private void searchBook(KeyEvent keyEvent) {
         String bookISBN = bookSearchField.getText();
         if (bookISBN.isEmpty()) {
-            Alerts.showInformationAlert("Field validation", "Book ISBN field is empty!");
+            errorISBNMessage.setValue("Book ISBN field is empty!");
+//            Alerts.showInformationAlert("Field validation", "Book ISBN field is empty!");
             return;
         }
 
         book = bookService.findByISBN(bookISBN);
 
         if (book == null) {
-            Alerts.showErrorAlert("Book not found", "Book with given ISBN does not exist!");
+            errorISBNMessage.setValue("Book with given ISBN does not exist!");
+//            Alerts.showErrorAlert("Book not found", "Book with given ISBN does not exist!");
             return;
         }
 
         if (book.getAvailability().equals("Not available")) {
-            Alerts.showErrorAlert("Book not available", "Book with given ISBN is not available!");
+            errorISBNMessage.setValue("Book with given ISBN is not available!");
+//            Alerts.showErrorAlert("Book not available", "Book with given ISBN is not available!");
             return;
         }
-
+        errorISBNMessage.setValue("");
 
         this.bookTitle.setText(book.getTitle());
         this.bookAuthor.setText(book.getAuthor());
@@ -85,42 +129,47 @@ public class IssueBookController extends ResizeableBaseController implements Ini
             this.issueBook.setDisable(false);
         }
     }
-
+    private void setUserControls(UserDTO user){
+        this.userFullName.setText(user.getFullname());
+        this.userEmail.setText(user.getEmail());
+        if (book != null && !numberOfDaysTextField.getText().isEmpty()) {
+            this.issueBook.setDisable(false);
+        }
+    }
     @FXML
     private void searchUser(KeyEvent keyEvent) {
         String userId = userSearchTextField.getText();
         if (userId.isEmpty()) {
-            Alerts.showInformationAlert("Field validation", "User ID field is empty!");
+            errorUserMessage.setValue("User ID field is empty!");
+//            Alerts.showInformationAlert("Field validation", "User ID field is empty!");
             return;
         }
-
-        user = userService.findById(Long.parseLong(userId));
-
+        this.userId = Long.parseLong(userId);
+        user = userService.findById(this.userId);
+//        System.out.println(user.getId());
         if (user == null) {
-            Alerts.showErrorAlert("User not found", "User with given ID does not exist!");
+            errorUserMessage.setValue("User with given ID does not exist!");
+//            Alerts.showErrorAlert("User not found", "User with given ID does not exist!");
             return;
         }
+        setUserControls(user);
 
-        this.userFullName.setText(user.getFullname());
-        this.userEmail.setText(user.getEmail());
-
-        if (book != null && !numberOfDaysTextField.getText().isEmpty()) {
-            this.issueBook.setDisable(false);
-        }
     }
 
     @FXML
     private void enterNumberOfDays(KeyEvent keyEvent) {
         String numberOfDays = numberOfDaysTextField.getText();
         if (numberOfDays.isEmpty()) {
-            Alerts.showInformationAlert("Field validation", "Number of days field is empty!");
+            errorUserMessage.setValue("Number of days field is empty!");
+//            Alerts.showInformationAlert("Field validation", "Number of days field is empty!");
             return;
         }
 
         try {
             Integer.parseInt(numberOfDays);
         } catch (NumberFormatException e) {
-            Alerts.showErrorAlert("Field validation", "Number of days field must be a number!");
+            errorUserMessage.setValue("Number of days field must be a number!");
+//            Alerts.showErrorAlert("Field validation", "Number of days field must be a number!");
         }
 
         if (book != null && user != null) {
@@ -145,6 +194,10 @@ public class IssueBookController extends ResizeableBaseController implements Ini
 
         Alerts.showInformationAlert("Book issued", "Book has been issued successfully!");
         this.clearFields();
+        if(logged){
+            this.setUserControls(user);
+        }
+
     }
 
     private void clearFields() {
